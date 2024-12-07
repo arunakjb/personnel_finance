6bb46f6945ddfd558f51f260650f37d5de1d7631
Arun Kumar P <arunjb639@gmail.com>
2024-12-07 11:03:32 +0530

    Add Components

---
+++ Finance.UI/src/account_component/logout-form/logout-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-form',
+  templateUrl: './logout-form.component.html',
+  styleUrl: './logout-form.component.css'
+})
+export class LogoutFormComponent {
+
+}
